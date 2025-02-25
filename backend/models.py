f676c956e078380826f118e58bb075a2606cf4e3
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-02-25 12:03:41 +0530

    Script backend models.py, register it in admin.py, makemigrations, migrate, createsuperusesr and runserver

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # Name of the genre (e.g., Rock, Pop)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
