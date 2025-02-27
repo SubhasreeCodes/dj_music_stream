370f19dd00d7a5b091f368fab42a3cbf1159b2b5
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-02-27 11:48:08 +0530

    Create "brand" backend\models.py

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,15 @@ class Genre(models.Model):
     class Meta:
         verbose_name = 'Genre'
         verbose_name_plural = 'Genres'
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # Name of the music streaming service
+    description = models.TextField(null=True, blank=True)  # A description about the brand
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
